6a979eb27a5cf6bd22ff7e0d9ee0184b9aa5b98f
Gauravsaha-97 <gauravsaha385@gmail.com>
2020-10-01 07:03:33 +0530

    Added smile detection feature in the code

--- FaceDetection/face.py
+++ FaceDetection/face.py
@@ -4,6 +4,7 @@ cap = cv2.VideoCapture(0)
 
 classifier = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 eye_classifier = cv2.CascadeClassifier('haarcascade_eye_tree_eyeglasses.xml')
+smile_cascade = cv2.CascadeClassifier('haarcascade_smile.xml') 
 
 while cap.isOpened():
 
@@ -21,6 +22,13 @@ while cap.isOpened():
             for eye in eyes:
                 ex,ey,ew,eh = eye
                 cv2.rectangle(eye_colored_region, (ex,ey),(ex+ew,ey+eh),(0,255,0),5)
+
+            smiles = smile_cascade.detectMultiScale(roi_gray, 1.8, 20) 
+  
+            for (sx, sy, sw, sh) in smiles: 
+                cv2.rectangle(roi_color, (sx, sy), ((sx + sw), (sy + sh)), (0, 0, 255), 2) 
+
+
         cv2.imshow("My cam", image)
 
     key = cv2.waitKey(1)
